f28d56225d588c63af7cd4dd430f1b7c19e66b47
saher016 <asaher016@gmail.com>
2025-10-08 05:43:15 +0300

    Update Layout.jsx

--- pulsepoint_website/src/components/pages/Layout.jsx
+++ pulsepoint_website/src/components/pages/Layout.jsx
@@ -1,6 +1,7 @@
 import {Outlet} from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../footer/Footer";
+import Doctors from "../Doctors/Doctors";
 export default function Layout() {
     return (
         <>
@@ -8,7 +9,8 @@ export default function Layout() {
             <main>
                 <Outlet />
             </main>
-        <Footer/>
+            <Doctors />
+            <Footer/>
         </>
     )
 }
