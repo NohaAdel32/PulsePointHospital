3cc52f768cb58a740ec065dbc86ed2d9af7f5b61
NohaAdel32 <136817046+NohaAdel32@users.noreply.github.com>
2025-10-11 21:09:12 +0300

    error page

--- pulsepoint_website/src/App.jsx
+++ pulsepoint_website/src/App.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/pages/layout.jsx";
 import Home from "./components/pages/Home.jsx";
-
+import ErrorPage from "./components/pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout />,
+        element: <Layout />, errorElement: <ErrorPage />,
         children: [
             { path: '/', element: <Home /> }
             
